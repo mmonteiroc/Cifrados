ea383b251e1d821447d111d74096ff3415365a9b
Miguel Monteiro Claveri <miguelmonteiroclaveri@gmail.com>
2018-11-21 15:02:15 +0100

    first commit

--- src/PlayFair.java
+++ src/PlayFair.java
@@ -1,34 +1,47 @@
-
+/**
+ * @author mmonteiro
+ * @project Cifrados
+ */
 public class PlayFair {
 
     public static void main(String[] args) {
-        encrypt("hola mundo","holaaaoekalee");
+        encrypt("hola mundo","holaaao  ekaleessxwq!");
     }
-    public static String decrypt(String text, String pass) {
 
-
-        return "";
-    }
 
     public static String encrypt(String text, String pass) {
-        String[] letras = { "A", "B", "C", "D", "E", "F", "G", "H", "i", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W","X", "Y", "Z"};
+        String[] letras = { "A", "B", "C", "D", "E", "F", "G", "H", "I", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W","X", "Y", "Z"};
         StringBuilder passwordCorrecta = new StringBuilder();
 
+        StringBuilder fraseLimpia = new StringBuilder();
+        pass = pass.toUpperCase();
+        // Aqui lo que hacemos es comprobar si lleva algun caracter especial la passwrd, en el caso de
+        // que sea asi, se eliminan los caracteres especiales
+        for (int i = 0; i < pass.length(); i++) {
+            if (pass.charAt(i) >= 65 && pass.charAt(i)<=90){
+                fraseLimpia.append(pass.charAt(i));
+            }else{
+                char c = caracterEspecial(pass.charAt(i));
+                if (c>= 65 && c<=90){
+                    fraseLimpia.append(c);
+                }
+            }
+        }
 
         //Aqui hacemos que la password no tenga ningun caracter repetido
         //EX: ARANYA = ARNY
-        for (int i = 0; i < pass.length(); i++) {
+        for (int i = 0; i < fraseLimpia.length(); i++) {
             StringBuilder c = new StringBuilder() ;
-            c.append(pass.charAt(i));
+            c.append(fraseLimpia.charAt(i));
             if (passwordCorrecta.toString().contains(c.toString())){
                 continue;
             }else {
-                passwordCorrecta.append(pass.charAt(i));
+                passwordCorrecta.append(fraseLimpia.charAt(i));
             }
         }
 
         //Pasamos todos los caracteres a mayusculas
-        StringBuilder passwdMAY = new StringBuilder(passwordCorrecta.toString().toUpperCase());
+        StringBuilder passwdMAY = new StringBuilder(passwordCorrecta.toString());
 
         String[][] matriz = new String[5][5];
         int filasIniciales = passwdMAY.length()/5;
@@ -52,8 +65,20 @@ public class PlayFair {
         int contador = 0;
         for (int i = 0; i < matriz.length; i++) {
             for (int j = 0; j < matriz[0].length; j++) {
-                if (matriz[i][j]!=null){
+                if (matriz[i][j]==null){
 
+                    if  (passwdMAY.toString().contains(letras[contador])){
+                        //Si contiene esa letra, no la escribimos y saltamos a la siguiente iteracion del bucle
+                        contador++;
+                        i--;
+                        j--;
+                        continue;
+                    }else {
+                        //Si no contiene esa letra escribimos en la matriz esa letra
+                        matriz[i][j] = letras[contador];
+                    }
+
+                    contador++;
                 }
             }
         }
@@ -63,6 +88,25 @@ public class PlayFair {
 
         return "";
     }
+
+    public static String decrypt(String text, String pass) {
+
+
+        return "";
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     public static char caracterEspecial (char a){
         switch (a){
